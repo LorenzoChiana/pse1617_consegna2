4d0dcd7c80c26f95609faaf260287fe105245de5
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2016-10-31 14:39:10 +0100

    Added and modified come classes

--- smart_toilet/smart_toilet.ino
+++ smart_toilet/smart_toilet.ino
@@ -1,4 +1,9 @@
+#include "Led.h"
 #include "LedExt.h"
+#include "MotionDetector.h"
+#include "PIRSensor.h"
+#include "ProximitySensor.h"
+#include "Sonar.h"
 
 #define T1_PIN 2
 #define T2_PIN 3
@@ -11,13 +16,26 @@
 
 int brightness;
 int fadeAmount;
+
+Light* led1;
+Light* led3;
 LightExt* led2;
+MotionDetector* pir;
+ProximitySensor* proxSensor;
 
 void setup() {
+  led1 = new Led(L1_PIN);
+  
   //inizializzazione sciacquone
   brightness = 0;
   fadeAmount = 5;
-  led2 = new LedExt(L2_PIN,brightness);
+  led2 = new LedExt(L2_PIN,brightness); 
+  
+  led3 = new Led(L3_PIN); 
+  
+  pir = new PIRSensor(PIR_PIN);
+  
+  proxSensor = new Sonar(ECHO_PIN, TRIG_PIN);
 }
 
 void loop() {
